f42893609de4c2ff5041581fd093bac3fc0ef0ff
SoraKumo <info@croud.jp>
2025-12-14 17:38:55 +0900

    first-commit

---
+++ src/index.ts
@@ -0,0 +1,100 @@
+import "dotenv/config";
+import { graphqlServer } from "@hono/graphql-server";
+import { serve } from "@hono/node-server";
+import SchemaBuilder from "@pothos/core";
+import DrizzlePlugin from "@pothos/plugin-drizzle";
+import { explorer } from "apollo-explorer/html";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { getTableConfig } from "drizzle-orm/pg-core";
+import { generate } from "graphql-auto-query";
+import { Hono } from "hono";
+import PothosDrizzleGeneratorPlugin from "pothos-drizzle-generator";
+import {
+  isOperation,
+  OperationMutation,
+  OperationQuery,
+} from "pothos-drizzle-generator";
+import { relations } from "./db/relations";
+
+const db = drizzle({
+  connection: process.env.DATABASE_URL!,
+  relations,
+  logger: true,
+});
+
+export interface PothosTypes {
+  DrizzleRelations: typeof relations;
+  Context: { userId?: string };
+}
+
+const builder = new SchemaBuilder<PothosTypes>({
+  plugins: [
+    DrizzlePlugin,
+    PothosDrizzleGeneratorPlugin, // Set plugin
+  ],
+  drizzle: {
+    client: () => db,
+    relations,
+    getTableConfig,
+  },
+  pothosDrizzleGenerator: {
+    // クエリの最大の深さ
+    depthLimit: () => 5,
+    // 使用しないテーブル
+    use: { exclude: ["postsToCategories"] },
+    models: {
+      users: {
+        // データの変更禁止
+        operations: { exclude: ["mutation"] },
+      },
+      // posts: {
+      //   // 上書き禁止フィールド
+      //   inputFields: { exclude: ["createdAt", "updatedAt"] },
+      //   // データ書き込み時は自分のIDを設定
+      //   inputData: ({ ctx }) => {
+      //     if (!ctx.userId) throw new Error("No permission");
+      //     return { authorId: ctx.userId };
+      //   },
+      //   where: ({ ctx, operation }) => {
+      //     // 抽出時は公開されているデータか、自分のデータ
+      //     if (isOperation(OperationQuery, operation)) {
+      //       return {
+      //         OR: [{ published: true }, { authorId: { eq: ctx.userId } }],
+      //       };
+      //     }
+      //     // 書き込み時は自分のデータ
+      //     if (isOperation(OperationMutation, operation)) {
+      //       return { authorId: ctx.userId };
+      //     }
+      //   },
+      // },
+    },
+  },
+});
+
+const schema = builder.toSchema({ sortSchema: false });
+
+const app = new Hono();
+// Apollo Explorer
+app.get("/", (c) => {
+  return c.html(
+    explorer({
+      initialState: {
+        // Set up sample GraphQL operations
+        document: generate(schema, 1),
+      },
+      endpointUrl: "/",
+      introspectionInterval: 10000,
+    })
+  );
+});
+app.post(
+  "/",
+  graphqlServer({
+    schema,
+  })
+);
+
+serve(app);
+
+console.log("http://localhost:3000");
